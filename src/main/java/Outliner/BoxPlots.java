dcd798e505696343e73de6dd5f4723c6186c583b
ElaineGxy <gaoxiyan2011@163.com>
2017-08-09 14:50:54 +0800

    热度检测

--- src/main/java/Outliner/BoxPlots.java
+++ src/main/java/Outliner/BoxPlots.java
@@ -22,21 +22,20 @@ public class BoxPlots {
 		this.sort();
 		int QLIndex=length/4;
 		int QUIndex=length-QLIndex;
-		QL=data[QLIndex+1];
+		QL=data[QLIndex];
 		QU=data[QUIndex];
 		IQR=QU-QL;
 		expUpper=QU+1.5*IQR;
 		expLower=QL-1.5*IQR;
 	}
 	/**
-	 * 1：热点事件 2：热点事件转换为非热点事件 3：非热点事件
+	 * 1：热点事件 2：热点事件转换为非热点事件 0：非热点事件
 	 * @return
 	 */
-	public int isOutliner() {
+	public boolean isOutliner() {
 		findQ();
 		System.out.println(this.currentData);
-		if(this.currentData>this.expUpper)return 1;
-		else if(this.currentData<this.expLower)return 2;
-		return 3;
+		if(this.currentData>this.expUpper)return true;
+		return false;
 	}
 }
